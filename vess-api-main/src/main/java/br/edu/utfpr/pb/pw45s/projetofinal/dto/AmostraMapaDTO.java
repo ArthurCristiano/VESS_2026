9bba24a52868c18efa953a3ca093c6e59bb4aac6
adriana <adrianadantas@alunos.utpr.edu.br>
2026-05-26 19:21:28 -0300

    add dto

---
+++ vess-api-main/src/main/java/br/edu/utfpr/pb/pw45s/projetofinal/dto/AmostraMapaDTO.java
@@ -0,0 +1,22 @@
+package br.edu.utfpr.pb.pw45s.projetofinal.dto;
+
+import br.edu.utfpr.pb.pw45s.projetofinal.model.Amostra;
+import lombok.Data;
+
+@Data
+public class AmostraMapaDTO {
+
+    private Long id;
+    private String nomeAmostra;
+    private String localizacao;
+    private AvaliacaoMapaResumoDTO avaliacao;
+
+    public static AmostraMapaDTO from(Amostra amostra) {
+        AmostraMapaDTO dto = new AmostraMapaDTO();
+        dto.setId(amostra.getId());
+        dto.setNomeAmostra(amostra.getNomeAmostra());
+        dto.setLocalizacao(amostra.getLocalizacao());
+        dto.setAvaliacao(AvaliacaoMapaResumoDTO.from(amostra.getAvaliacao()));
+        return dto;
+    }
+}
